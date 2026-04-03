c1b851cc5a4160a6077ac5a638f9def90e496ca9
Savallejoay <savallejoay@uide.edu.ec>
2026-04-02 21:05:15 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI, HTTPException
+from sqlmodel import SQLModel, Field, create_engine, Session, select
+
+app = FastAPI()
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    username: str
+    password: str
+
+engine = create_engine("sqlite:///database.db")
+
+def create_db_and_users():
+    SQLModel.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        users = [
+            User(username="Santos", password="uide.2026"),
+            User(username="James", password="ciberseguridad"),
+            User(username="Hollow", password="Knight")
+        ]
+
+        for user in users:
+            existing = session.exec(
+                select(User).where(User.username == user.username)
+            ).first()
+
+            if not existing:
+                session.add(user)
+
+        session.commit()
+
+# 👉 Esto hace que se cree la DB al iniciar
+@app.on_event("startup")
+def on_startup():
+    create_db_and_users()
+
+# -----------------------------
+# LOGIN
+# -----------------------------
+@app.post("/login")
+def login(user: User):
+    with Session(engine) as session:
+        db_user = session.exec(
+            select(User).where(User.username == user.username)
+        ).first()
+
+        if db_user and db_user.password == user.password:
+            return {"message": "Login exitoso"}
+
+    raise HTTPException(status_code=401, detail="Usuario o Contraseña incorrectos")
